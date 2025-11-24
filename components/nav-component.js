42efd75eed675f4fa6ef7dec5fa2017deaa6130a
viktor webengineering <viktor.webengineering>
2025-11-24 15:09:44 -0600

    update routes

--- components/nav-component.js
+++ components/nav-component.js
@@ -6,11 +6,11 @@ const element = `
       <nav role="navigation">
         <ul class="nav-links">
           <li><a href="/">Inicio</a></li>
-          <li><a href="/about/about.html">Acerca de mi</a></li>
-          <li><a href="/services/services.html">Servicios</a></li>
-          <li><a href="/meditations/meditations.html">Meditaciones</a></li>
-          <li><a href="/blog/blog.html">Blog</a></li>
-          <li><a href="/contact/contact.html">Contacto</a></li>
+          <li><a href="about/about.html">Acerca de mi</a></li>
+          <li><a href="services/services.html">Servicios</a></li>
+          <li><a href="meditations/meditations.html">Meditaciones</a></li>
+          <li><a href="blog/blog.html">Blog</a></li>
+          <li><a href="contact/contact.html">Contacto</a></li>
         </ul>
       </nav>
     </div>
